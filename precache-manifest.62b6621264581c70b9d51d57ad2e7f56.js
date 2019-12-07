73e2408c95caecc3769687e01cf2083edfca9219
impressiveHen <b02505002@gmail.com>
2019-12-06 18:12:21 -0800

    Updates

---
+++ precache-manifest.62b6621264581c70b9d51d57ad2e7f56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0217fdaeb722a210621a2cbe5a005686",
+    "url": "/Chat-Room/index.html"
+  },
+  {
+    "revision": "ff83e53d21bc5a89cdf0",
+    "url": "/Chat-Room/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ca43c9783c221392439c",
+    "url": "/Chat-Room/static/js/2.1359d4fa.chunk.js"
+  },
+  {
+    "revision": "ff83e53d21bc5a89cdf0",
+    "url": "/Chat-Room/static/js/main.fcd1d54c.chunk.js"
+  },
+  {
+    "revision": "c7b6d27f6054a3ff4537",
+    "url": "/Chat-Room/static/js/runtime-main.6e2550f1.js"
+  }
+]);
